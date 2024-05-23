4dfef66b09c8981cbba0c5edc092161832adc441
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2024-05-23 11:31:02 -0300

    Create estrutura no

---
+++ tipoNo.h
@@ -0,0 +1,41 @@
+#ifndef TIPONO_H
+#define TIPONO_H
+
+typedef enum {typeInt, typeFloat, typeString, typeId, typeOpr} eTipo;
+
+typedef struct{
+    int val;
+} intTipoNo;
+
+typedef struct{
+    float val;
+} floatTipoNo;
+
+typedef struct{
+    char *str;
+} stringTipoNo;
+
+typedef struct{
+    char *name; // Nome da Variavel
+    int i; // Indice do vetor de simbolos
+} idTipoNo;
+
+typedef struct{
+    int opr; // Operacao
+    int nOps; // Numero de operandos
+    struct tagTipoNo *op[1]; // Operandos
+} oprTipoNo;
+
+typedef struct tagTipoNo{
+    eTipo type;
+
+    union{
+        intTipoNo inteiro;
+        floatTipoNo real;
+        stringTipoNo string;
+        idTipoNo id;
+        oprTipoNo opr;
+    };
+} tipoNo;
+
+#endif
